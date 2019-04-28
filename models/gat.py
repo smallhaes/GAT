2429973e37dc33d8bcd3d1a9e4c6df48f4353fc6
littlehaes <506575628@qq.com>
2019-04-28 17:00:45 +0800

    增加注释

--- models/gat.py
+++ models/gat.py
@@ -8,20 +8,24 @@ class GAT(BaseGAttN):
     def inference(inputs, nb_classes, nb_nodes, training, attn_drop, ffd_drop,
             bias_mat, hid_units, n_heads, activation=tf.nn.elu, residual=False):
         attns = []
+        # 遍历第一个attention抽头
         for _ in range(n_heads[0]):
             attns.append(layers.attn_head(inputs, bias_mat=bias_mat,
                 out_sz=hid_units[0], activation=activation,
                 in_drop=ffd_drop, coef_drop=attn_drop, residual=False))
         h_1 = tf.concat(attns, axis=-1)
+        # 遍历第2个hid_units至倒数第2个hid_units, 每个hid_units对应一个attention抽头？
         for i in range(1, len(hid_units)):
             h_old = h_1
             attns = []
+            # 对每个hid_units进行遍历
             for _ in range(n_heads[i]):
                 attns.append(layers.attn_head(h_1, bias_mat=bias_mat,
                     out_sz=hid_units[i], activation=activation,
                     in_drop=ffd_drop, coef_drop=attn_drop, residual=residual))
             h_1 = tf.concat(attns, axis=-1)
         out = []
+        # 遍历最后一个attention抽头
         for i in range(n_heads[-1]):
             out.append(layers.attn_head(h_1, bias_mat=bias_mat,
                 out_sz=nb_classes, activation=lambda x: x,
